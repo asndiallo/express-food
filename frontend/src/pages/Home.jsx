fd921d6f8db241a2ccda715a46ed244ca063844d
Ging <YanM16M@gmail.com>
2023-01-24 14:16:43 +0100

    dishes and desserts added to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from '../components/Menu';
+import axios from 'axios';
+import { domain } from '../variables';
 import './Home.css';
 
 function Home() {
+    const [dishes, setDishes] = useState([]);
+    const [desserts, setDesserts] = useState([]);
+
+    useEffect(() => {
+        // On récupère les menus du jour (plats, desserts)
+        getTodayMenu()
+    }, [])
+
+    const getTodayMenu = async () => {
+        const link = domain + "/api/v1/menus";
+        if (link !== '') {
+            await axios.get(link, {})
+            .then(function(res) {
+                const data = res.data;
+                if (data.today_dishes !== undefined) {
+                    setDishes(data.today_dishes);
+                }
+                if (data.today_desserts !== undefined) {
+                    setDesserts(data.today_desserts);
+
+                }
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        } 
+    }
+
+    const dispatchData = () => {
+
+    }
+
     return (  
         <div className="home_page">
             <div 
@@ -16,13 +50,26 @@ function Home() {
                 <div className="dish">
                     <h1>Plats du jour :</h1>
                     <div className="dish_content">
-                        <Menu />
+                        {
+                            dishes.map((item) => {
+                                return (
+                                    <Menu key={item._id} data={item} />
+                                )
+                            })
+                        }
                     </div>
                 </div>
                 <div className="dessert">
                     <h1>Desserts du jour :</h1>
-                    <div className="dessert_content"></div>
-                    
+                    <div className="dessert_content">
+                    {
+                            desserts.map((item) => {
+                                return (
+                                    <Menu key={item._id} data={item} />
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
